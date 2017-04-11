8520967915110933d36adf0d7dad3b606dea7a13
Paul Dominic Pacquing <dominicpacquing@gmail.com>
2017-04-11 21:17:41 +1000

    Redefine state monad

--- src/main/scala/monad/StateMonad/StateMonad.scala
+++ src/main/scala/monad/StateMonad/StateMonad.scala
@@ -3,32 +3,38 @@ package monad.state
 object StateMonad extends App {
   type Stack[A] = List[A]
 
-  case class State[S, +A](run: S => (A, S)) {
-    def map[B](f: A => B): State[S, B] = State(s => {
-      val (a, s1) = run(s)
-      (f(a), s1)
-    })
+  trait State[S, A] {
+    def apply(s:S): (A, S)
+    
+    def map[B](f:A =>B): State[S, B] = State { s =>
+     val (a, newState) = this(s)
+     (f(a), newState)
+    }
 
-    def flatMap[B](f: A => State[S, B]): State[S, B] = State(s => {
-      val (a, s1) = run(s)
-      val fn = f(a)
-      fn.run(s1)
-    })
+    def flatMap[B](f:A => State[S, B]): State[S, B] = State { s =>
+     val (a, newState) = this(s)
+     f(a)(newState)
+    }
   }
 
-  def push[A](a: A): State[Stack[A], Unit] = State(s => ((), a :: s))
+  object State {
+    def apply[S, A](r: S => (A, S)): State[S, A] = new State[S, A] {
+      def apply(s:S) = r(s)
+    }
+  }
 
-  def pop[A]: State[Stack[A], Option[A]] = State({
-    case head :: tail => (Some(head), tail)
-    case Nil => (None, Nil)
-  })
+  def push[A](a:A) : State[Stack[A], Unit] = State { stack => ((), a :: stack) }
 
-  def popPairs[A]: State[Stack[A], (Option[A], Option[A])] =
-    pop[A].flatMap(pop1 => pop[A].map(pop2 => (pop1, pop2)))
+  def pop[A]: State[Stack[A], Option[A]] = State {
+    case a :: tail => (Some(a), tail)
+    case Nil       => (None, Nil)
+  }
 
-  val initStack = List(1,2,3,4)
+  def popPairs[A]: State[Stack[A],(Option[A], Option[A])] =
+    pop[A].flatMap(opt1 => pop[A].map(opt2 => (opt1, opt2) ))
 
-  println(popPairs.run(initStack))
+  println(pop(List(1,2,3,4)))
+  println(push(2)(List(5,6,7)))
+  println(popPairs(List(1,2,3,4)))
 
-  push(2).run(List(5,6,7))
 }
