a617c57bd5da76936c0b6e2d270a37163c72057f
Paul Dominic Pacquing <dominicpacquing@gmail.com>
2017-04-11 21:43:36 +1000

    Restored previous implementation of state monad

---
+++ src/main/scala/monad/StateMonad/StateMonad2.scala
@@ -0,0 +1,37 @@
+package monad.state
+
+object StateMonad2 extends App {
+  type Stack[A] = List[A]
+
+  case class State[S, +A](run: S => (A, S)) {
+    def map[B](f: A => B): State[S, B] = State(s => {
+      val (a, s1) = this.run(s)
+      (f(a), s1)
+    })
+
+    def flatMap[B](f: A => State[S, B]): State[S, B] = State(s => {
+      val (a, s1) = this.run(s)
+      val fn = f(a)
+      fn.run(s1)
+    })
+  }
+
+  def push[A](a: A): State[Stack[A], Unit] = State(s => ((), a :: s))
+
+  def pop[A]: State[Stack[A], Option[A]] = State({
+    case head :: tail => (Some(head), tail)
+    case Nil => (None, Nil)
+  })
+
+  def pop2[A]: State[Stack[A], Option[A]] =
+    pop[A].map(pop1 => (pop1))
+
+  def popPairs[A]: State[Stack[A], (Option[A], Option[A])] =
+    pop[A].flatMap(pop1 => pop[A].map(pop2 => (pop1, pop2)))
+
+  val initStack = List(1,2,3,4)
+
+  println(popPairs.run(initStack))
+
+  push(2).run(List(5,6,7))
+}
